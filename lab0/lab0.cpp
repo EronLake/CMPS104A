6ec914dccfd812c33ab4b932ebe5c1ff05e6d4b3
Eron Lake <ejlake@ucsc.edu>
2016-09-29 14:11:04 -0700

    added labO

---
+++ lab0/lab0.cpp
@@ -0,0 +1,19 @@
+/////////////////////////////////////////////
+//Lab0
+//Eron Lake
+//ejlake@ucsc.edu
+//CMPS104A
+//Program for introduction/refresher on c++/c
+//////////////////////////////////////////////
+
+//include statements 
+
+#include <iostream>
+
+//main function
+int main()
+{
+   //prints "Hello World"
+   std::cout << "Hello World!" << std::endl ;
+   return 0;
+}
